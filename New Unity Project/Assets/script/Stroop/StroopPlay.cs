e8dbde11d221d8d2f2e16d62bd6c783efc9718f3
faulty337 <faulty337@gmail.com>
2020-08-14 20:38:29 +0900

    stroop 저장형태

--- New Unity Project/Assets/script/Stroop/StroopPlay.cs
+++ New Unity Project/Assets/script/Stroop/StroopPlay.cs
@@ -7,16 +7,29 @@ public class StroopPlay : MonoBehaviour
 {
     public Text Question;
     public GameObject manager;
-    public string[] color = {"빨간", "노란", "초록", "파란"};
-    public string[] colorNum = {"<color=#bf2836>", "<color=#f3c500>", "<color=#0c7b3f>", "<color=#004e9e>"};
+    public string[] color = {"빨강", "노랑", "초록", "파랑"};
+    public string[] colorstr = {"빨간", "노란", "초록", "파란"};
+    public string[] colorNum;
     public int stage, totalstage;
+    private int level;
     public List<string[]> Data = new List<string[]>();
+    private string[] reactionTime, Q, input, Answer;
     public float time, Qtime;
     // Start is called before the first frame update
     void Start()
     {
+        colorNum[0] = "<color=#bf2836>";
+        colorNum[0] = "<color=#f3c500>";
+        colorNum[0] = "<color=#0c7b3f>";
+        colorNum[0] = "<color=#004e9e>";
+        level = GameManager.Level;
+        //{"<color=#bf2836>", "<color=#f3c500>", "<color=#0c7b3f>", "<color=#004e9e>"};
         Data = manager.GetComponent<StroopManager>().Data.ConvertAll(s => s);
         totalstage = 40;
+        reactionTime = new string[totalstage];
+        Q = new string[totalstage];
+        input = new string[totalstage];
+        Answer = new string[totalstage];
         Qtime = 1.0f;
         //colorNum[1] + "aaa" + "</color>"
         NextQuestion();
@@ -38,16 +51,40 @@ public class StroopPlay : MonoBehaviour
     }
 
     public void NextQuestion(){
+        stage++;
         int ran = Random.Range(0,Data.Count);
         int ran2 = Random.Range(0,2);
-        int ran3 = Random.Range(0,4);
-        print(colorNum[2]);
+        int fake = Random.Range(0,4);
+        int ans = Random.Range(0,4);
         Question.color = UnityEngine.Color.blue;
-        Question.text = color[ran3]+Data[ran][ran2];
-        //colorNum[ran3]++"</color>"
-        stage++;
+        Question.text = colorNum[fake]+colorstr[ans]+Data[ran][ran2]+"</color>";
+        input[stage] = "Pass";
+        Q[stage] = Data[ran][ran2];
+        switch (level){
+            case 1 :
+                Answer[stage] = colorstr[ans];
+                break;
+            case 2 :
+                if(ran2 == 1)
+                    Answer[stage] = colorstr[stage];
+                else
+                    Answer[stage] = "Pass";
+                break;
+            case 3 :
+                if(ran2 == 0)
+                    Answer[stage] = colorstr[stage];
+                else
+                    Answer[stage] = "Pass";
+                break;
+        }
+        //
+        
         // if(stage > totalstage){
         //     manager.GetComponent<StroopManager>().gameEnd();
         // }
+    }
+
+    public void record(){
+
     }
 }
