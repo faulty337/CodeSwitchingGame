b22068688c3256009f751560da2cb725a03f75c1
faulty337 <faulty337@gmail.com>
2020-09-26 18:29:56 +0900

    단어장 제작중

---
+++ CodeSwitching/Assets/script/WordListScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordListScript : MonoBehaviour
+{
+    public Text Lan1, Lan2;
+    public GameObject WordContent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    public void ContentUpdata(string lan1, string lan2){
+        Lan1.text = lan1;
+        Lan1.text = lan2;
+    }
+}
---
+++ CodeSwitching/Assets/script/Wordlist.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wordlist : MonoBehaviour
+{
+    public GameObject WordList_1, WordList_2;
+    public GameObject PfWordCotent;
+
+    private List<GameObject> wordlist;
+    // Start is called before the first frame update
+
+    public void WordSet(List<string[]> Data){
+        wordlist = new List<GameObject>();
+        bool sw = true;
+        foreach(string[] word in Data){
+            var WordContent = new GameObject();
+            if(sw){
+                WordContent = Instantiate(PfWordCotent, WordList_1.transform);
+            }else{
+                WordContent = Instantiate(PfWordCotent, WordList_2.transform);
+            }
+            WordContent.GetComponent<WordListScript>().ContentUpdata(word[0], word[1]);
+            WordContent.SetActive(true);
+            sw = !sw;
+            wordlist.Add(WordContent);
+        }
+    }
+    public void WordListReset(){
+        foreach(GameObject Con in wordlist){
+            Con.SetActive(false);
+            Destroy(Con);
+        }
+        wordlist.Clear();
+    }
+
+}
